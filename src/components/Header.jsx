d8184eac016bf4573dd095187ba99913a128bed4
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-01-30 22:33:38 +0700

    refactor: Change into mobile first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,39 @@
 import logo from "../images/logo.svg";
-import backgroundHero from "../images/desktop/image-hero.jpg";
+import hamburger from "../images/icon-hamburger.svg";
+import backgroundHero from "../images/mobile/image-hero.jpg";
 
 const Header = () => {
   return (
-    // Header
     <header
-      className="pt-16 pb-36 bg-center bg-cover"
+      className="pt-10 pb-56 px-6 bg-center bg-cover"
       style={{ backgroundImage: `url(${backgroundHero})` }}
     >
       <div className="container mx-auto">
         <nav className="flex justify-between items-center">
           {/* logo */}
-          <div>
+          <div className="logo">
             <img src={logo} alt="Loopstudios logo" />
           </div>
 
           {/* menu */}
-          <div className="flex flex-row space-x-8 text-white">
+          {/* <div className="sm:hidden md:hidden flex flex-row space-x-8 text-white">
             <a href="#about">About</a>
             <a href="#careers">Careers</a>
             <a href="#events">Events</a>
             <a href="#products">Products</a>
             <a href="#support">Support</a>
+          </div> */}
+
+          {/* hamburger */}
+          <div className="hamburger sm:block md:hidden">
+            <img src={hamburger} alt="Hamburger icon" />
           </div>
         </nav>
 
         {/* Hero */}
-        <div className="mt-32">
-          <div className="border-2 w-5/12 pt-11 pr-20 pb-11 pl-11">
-            <p className="text-6xl text-white uppercase">
+        <div className="mt-40">
+          <div className="border-2 w-full p-6">
+            <p className="josefin text-4xl text-white uppercase">
               Immersive experiences that deliver
             </p>
           </div>
