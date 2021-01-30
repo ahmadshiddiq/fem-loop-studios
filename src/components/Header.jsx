7e6ecd29ef57d9dd5dfff958578810457467a7d4
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-01-31 00:40:51 +0700

    feat: Add responsive mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,8 @@ import backgroundHero from "../images/mobile/image-hero.jpg";
 
 const Header = () => {
   return (
-    <header
-      className="pt-10 pb-56 px-6 bg-center bg-cover"
-      style={{ backgroundImage: `url(${backgroundHero})` }}
-    >
-      <div className="container mx-auto">
+    <header className="bg-center bg-cover">
+      <div className="pt-10 pb-56 px-6 lg:pt-16 lg:pb-36 lg:px-40">
         <nav className="flex justify-between items-center">
           {/* logo */}
           <div className="logo">
@@ -16,24 +13,24 @@ const Header = () => {
           </div>
 
           {/* menu */}
-          {/* <div className="sm:hidden md:hidden flex flex-row space-x-8 text-white">
+          <div className="hidden md:block flex flex-row space-x-8 text-white">
             <a href="#about">About</a>
             <a href="#careers">Careers</a>
             <a href="#events">Events</a>
             <a href="#products">Products</a>
             <a href="#support">Support</a>
-          </div> */}
+          </div>
 
           {/* hamburger */}
-          <div className="hamburger sm:block md:hidden">
+          <div className="hamburger cursor-pointer md:hidden">
             <img src={hamburger} alt="Hamburger icon" />
           </div>
         </nav>
 
         {/* Hero */}
         <div className="mt-40">
-          <div className="border-2 w-full p-6">
-            <p className="josefin text-4xl text-white uppercase">
+          <div className="border-2 w-full p-6 lg:w-5/12">
+            <p className="josefin text-4xl md:text-6xl lg:text-7xl text-white uppercase">
               Immersive experiences that deliver
             </p>
           </div>
