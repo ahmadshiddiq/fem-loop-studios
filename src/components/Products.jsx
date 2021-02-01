9402c51e27ff130bd343e9d4c59b97276f980b0f
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-02-01 19:40:46 +0700

    fix: responsive products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,23 +4,25 @@ const Products = () => {
   return (
     <section id="products">
       <div className="container mx-auto">
-        <div className="px-6 pt-1.5 pb-24">
-          {/* maybe delete this flex later */}
-          <div className="flex flex-col">
-            <h2 className="josefin uppercase text-center text-3xl mb-14 order-1">
+        <div className="px-6 pt-1.5 pb-24 lg:px-40">
+          <div className="flex flex-col lg:flex-row lg:flex-wrap lg:items-center justify-between">
+            <h2 className="josefin uppercase text-center text-3xl mb-14 order-1 lg:mb-0">
               Our creations
             </h2>
 
-            <div className="text-center order-3">
+            <div className="text-center order-3 lg:order-2">
               <a
                 href="#products"
-                className="josefin uppercase inline-block py-3.5 px-10 border-2 border-black mt-8"
+                className="josefin uppercase inline-block py-3.5 px-10 border-2 border-black mt-8 lg:mt-0"
               >
                 See all
               </a>
             </div>
 
-            <div className="order-2 grid gap-y-6 lg:grid-cols-4 lg:gap-7">
+            <div
+              id="product-wrapper"
+              className="order-2 grid gap-y-6 lg:grid-cols-4 lg:gap-7 lg:order-3 lg:mt-12"
+            >
               <ProductItem name="Deep Earth" />
               <ProductItem name="Night Arcade" />
               <ProductItem name="Soccer team VR" />
