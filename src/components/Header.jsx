888408f341172fd52be010081b61371e6c961dd8
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-02-01 09:30:02 +0700

    refactor: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,13 @@
-import logo from "../images/logo.svg";
-import hamburger from "../images/icon-hamburger.svg";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 const Header = () => {
   return (
     <header className="bg-center bg-cover">
-      <div className="container mx-auto pt-10 pb-56 px-6 lg:pt-16 lg:pb-36 lg:px-40">
-        <nav className="flex justify-between items-center">
-          {/* logo */}
-          <div className="logo">
-            <img src={logo} alt="Loopstudios logo" />
-          </div>
-
-          {/* menu */}
-          <div className="hidden flex-row space-x-8 text-white md:flex">
-            <a href="#about">About</a>
-            <a href="#careers">Careers</a>
-            <a href="#events">Events</a>
-            <a href="#products">Products</a>
-            <a href="#support">Support</a>
-          </div>
-
-          {/* hamburger */}
-          <div className="hamburger cursor-pointer md:hidden">
-            <img src={hamburger} alt="Hamburger icon" />
-          </div>
-        </nav>
-
-        {/* Hero */}
-        <div className="mt-40">
-          <div className="border-2 w-full p-6 md:w-8/12 lg:w-5/12">
-            <h1 className="josefin text-4xl text-white uppercase md:text-5xl">
-              Immersive experiences that deliver
-            </h1>
-          </div>
+      <div className="container mx-auto">
+        <div className="pt-10 pb-56 px-6 lg:pt-16 lg:pb-36 lg:px-40">
+          <Navbar />
+          <Hero />
         </div>
       </div>
     </header>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div id="hero" className="mt-40 lg:mt-32">
+      <div className="border-2 w-full p-6 lg:w-7/12 lg:pl-12 lg:py-11">
+        <h1 className="josefin text-white uppercase">
+          Immersive experiences that deliver
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
