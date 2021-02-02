eaef75f2b81683ac506a48cf72c65867c550b348
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-02-02 16:36:11 +0700

    feat: footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Products from "./components/Products";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <About />
       <Products />
+      <Footer />
     </>
   );
 }
