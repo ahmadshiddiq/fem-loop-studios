1f197ecdda336527b59c4ab03196dc8d9e5cd1af
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-02-05 12:14:32 +0700

    Restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Header from "./components/Header";
-import About from "./components/About";
-import Products from "./components/Products";
-import Footer from "./components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Header />
-      <About />
-      <Products />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
