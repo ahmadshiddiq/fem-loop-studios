a090df99a4645b8df7262c62d6519b44caf5b07b
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-02-10 23:58:07 +0700

    feat: add interactive to header button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import logo from "./images/logo.svg";
 import hamburger from "./images/icon-hamburger.svg";
 import close from "./images/icon-close.svg";
@@ -26,17 +28,27 @@ const pages = [
 ];
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <>
       <header className="bg-center bg-cover">
         <div className="px-6 pt-10 pb-56">
-          <div className="flex items-center justify-between">
+          <div className="relative flex items-center justify-between z-10">
             <div className="h-6">
               <img src={logo} alt="Loopstudios" className="h-full" />
             </div>
+
             <div className="h-6 w-6 flex items-center justify-center">
-              <img src={hamburger} alt="Hamburger" />
+              <button className="focus:outline-none" onClick={handleToggle}>
+                <img src={`${toggle ? hamburger : close}`} alt="Hamburger" />
+              </button>
             </div>
+
             <div className="hidden space-x-5">
               {pages.map((page, index) => (
                 <a key={index} href={page.link}>
@@ -45,6 +57,28 @@ function App() {
               ))}
             </div>
           </div>
+
+          {/* set to hidden for sec */}
+          <div
+            className={`${
+              toggle ? "hidden" : "block"
+            } absolute bg-black top-0 bottom-0 left-0 right-0`}
+          >
+            <div className="flex items-center h-full">
+              <div className="flex flex-col space-y-4 px-6">
+                {pages.map((page, index) => (
+                  <a
+                    key={index}
+                    href={page.link}
+                    className="josefin text-2xl text-white"
+                  >
+                    {page.title}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+
           <div className="mt-40 p-6 border-2 border-white">
             <h1 className="josefin text-4xl text-white">
               Immersive experiences that deliver
@@ -57,8 +91,3 @@ function App() {
 }
 
 export default App;
-
-// Cara looping data dengan map
-// {pages.map((page) => (
-//   <p>{page.title}</p>
-// ))}
